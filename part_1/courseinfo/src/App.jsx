e924a3863b711e27a34a11bfb92bdc30f8ebac62
Joaquin Matias Castellano <jcastellano@fullstack-open.optix-fibra.com.ar>
2023-08-20 20:53:11 -0300

    Course information step3

--- part_1/courseinfo/src/App.jsx
+++ part_1/courseinfo/src/App.jsx
@@ -9,7 +9,7 @@ const Content = (props) => {
     <div>
       <Part name={props.parts[0].name} exercises={props.parts[0].exercises} />
       <Part name={props.parts[1].name} exercises={props.parts[1].exercises} />
-      <Part name={props.parts[2].name} exercises={props.parts[2].exercises} />      
+      <Part name={props.parts[2].name} exercises={props.parts[2].exercises} />         
     </div>
   )
 }
@@ -30,17 +30,26 @@ const App = () => {
 
   const course = 'Half Stack application development'
 
-  const parts = [
-    { name: 'Fundamentals of React', exercises: 10 },
-    { name: 'Using props to pass data', exercises: 7 },
-    { name: 'State of a component', exercises: 14 }
-  ]
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  }
 
   return (
     <div>
       <Header course={course} />      
-      <Content parts={parts}/>
-      <Total parts={parts}/>
+      <Content parts={[part1, part2, part3]} />
+      <Total parts={[part1, part2, part3]}/>
     </div>
   )
 }
