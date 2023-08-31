9b2890b6eeaf663b969210d266ae6ab41f8ad1da
Joaquin Matias Castellano <jcastellano@fullstack-open.optix-fibra.com.ar>
2023-08-31 19:13:43 -0300

    Part 1.7, Unicafe step2

--- part_1/unicafe/src/App.jsx
+++ part_1/unicafe/src/App.jsx
@@ -1,27 +1,85 @@
 import { useState } from 'react'
 
 const App = () => {
-  // save clicks of each button to its own state
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
 
+  const [feedback, setFeedback] = useState({
+
+    good: 0, neutral: 0, bad: 0, all: 0, average: 0, positive: 0
+
+  })
+
+  const handleGoodFeedback = () => {
+
+    const newGoodValue = feedback.good + 1
+    const newAllValue = feedback.all + 1
+
+    const newFeedback = { 
+      ...feedback,      
+      good: newGoodValue,
+      all: newAllValue,
+      average: (newGoodValue - feedback.bad) / newAllValue,
+      positive: newGoodValue / newAllValue
+    }
+
+    setFeedback(newFeedback)
+
+  }
+
+  const handleNeutralFeedback = () => {
+
+    const newNeutralValue = feedback.neutral + 1
+    const newAllValue = feedback.all + 1
+
+    const newFeedback = { 
+      ...feedback,
+      neutral: newNeutralValue,
+      all: newAllValue,
+      average: (feedback.good - feedback.bad) / newAllValue,
+      positive: feedback.good / newAllValue
+    }
+
+    setFeedback(newFeedback)
+
+  }
+
+  const handleBadFeedback = () => {
+
+    const newBadValue = feedback.bad + 1
+    const newAllValue = feedback.all + 1
+
+    const newFeedback = { 
+      ...feedback,
+      bad: newBadValue,
+      all: newAllValue,
+      average: (feedback.good - newBadValue) / newAllValue,
+      positive: feedback.good / newAllValue
+    }
+
+    setFeedback(newFeedback)
+
+  }
+  
   return (
+    
     <div>
       <h1>give feedback</h1>
 
-      <button onClick={() => setGood(good + 1)}>good</button>
-      <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
-      <button onClick={() => setBad(bad + 1)}>bad</button>
+      <button onClick={handleGoodFeedback}>good</button>
+      <button onClick={handleNeutralFeedback}>neutral</button>
+      <button onClick={handleBadFeedback}>bad</button>
 
       <h1>statistics</h1>
 
-      <p>good {good}</p>
-      <p>neutral {neutral}</p>
-      <p>bad {bad}</p>
-
+      <p>good {feedback.good}</p>
+      <p>neutral {feedback.neutral}</p>
+      <p>bad {feedback.bad}</p>
+      <p>all {feedback.all}</p>
+      <p>average {feedback.average}</p>
+      <p>positive {feedback.positive}</p>
+      
     </div>
   )
+
 }
 
 export default App
