6360e7f12f1cf6fe8374658cfbad410f09fae7dc
Joaquin Matias Castellano <jcastellano@fullstack-open.optix-fibra.com.ar>
2023-09-11 17:06:12 -0300

    Part 2.1: Course information step6

---
+++ part_2/courseinfo/src/App.jsx
@@ -0,0 +1,66 @@
+const Header = ({ course }) => <h1>{course.name}</h1>
+
+const Parts = ({ course }) => {
+
+  return(
+
+    course.parts.map(part => 
+      <p key={part.id}>
+        {part.name} {part.exercises}
+      </p>
+    )
+
+  )
+
+}
+
+const Content = ({ course }) => {
+
+  return(
+    <ul>
+      <Parts course={course} />
+    </ul>
+
+  )
+}
+
+const Course = ({ course }) => {
+
+  return(
+    
+    <>
+      <Header course={course} />
+      <Content course={course} />
+    </>
+
+  )
+  
+}
+
+const App = () => {
+  const course = {
+    id: 1,
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      }
+    ]
+  }
+
+  return <Course course={course} />
+}
+
+export default App
