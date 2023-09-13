6542393a565d267b6825f6c592228730b08c8ac7
Joaquin Matias Castellano <jcastellano@fullstack-open.optix-fibra.com.ar>
2023-09-13 20:07:19 -0300

    Part 2.11: The Phonebook Step6

--- part_2/phonebook/src/App.jsx
+++ part_2/phonebook/src/App.jsx
@@ -1,4 +1,5 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 import Filter from './components/Filter'
 import PersonForm from './components/PersonForm'
 import Persons from './components/Persons'
@@ -9,12 +10,18 @@ const App = () => {
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
 
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ]) 
+  const [persons, setPersons] = useState([]) 
+
+  useEffect(() => {
+    console.log('effect')
+    axios
+      .get('http://192.168.27.251:3001/persons')
+      .then(response => {
+        console.log('promise fulfilled')
+        setPersons(response.data)
+        setPersonsToShow(response.data)
+      })
+  }, [])
 
   let [personsToShow, setPersonsToShow] = useState(persons)
 
